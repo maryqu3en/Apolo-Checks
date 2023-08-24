a1d2db1f9ee82f4f1b8048102ee30dbe65645ceb
Meriem Soubih <soubih.meriem@gmail.com>
2023-08-24 23:37:48 +0100

    project update

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const PORT = 4000;
 
 app.set("view engine", "pug");
-app.set("views", "/views");
+app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const tasks = [];
 
+app.get("/", (req, res) => {
+    res.render('dashboard', {tasks});
+  });
 
+app.post('/add', (req, res) => {
+  const task = req.body.task;
+  tasks.push({
+    title : task,
+    completed : false,
+  });
+  res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on http://localhost:${PORT}.`);
+  });
