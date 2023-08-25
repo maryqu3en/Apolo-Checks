2d83c172486f2b1cf202a5d47df09e4f1f1699be
Meriem Soubih <soubih.meriem@gmail.com>
2023-08-25 22:24:45 +0100

    project update

--- app.js
+++ app.js
@@ -12,18 +12,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const tasks = [];
 
 app.get("/", (req, res) => {
-    res.render('dashboard', {tasks});
-  });
+  res.render("dashboard", { tasks });
+});
 
-app.post('/add', (req, res) => {
+app.post("/add", (req, res) => {
   const task = req.body.task;
+  const id = Math.floor(Math.random() * 1000);
   tasks.push({
-    title : task,
-    completed : false,
+    title: task,
+    id: id,
   });
-  res.redirect('/');
+  res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const index = tasks.findIndex((post) => post.id == id);
+  tasks.splice(index, 1);
+  return res.redirect("/");
+});
+
+app.get("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = tasks.find((post) => post.id == id);
+  return res.render("edit", {
+    todo,
+  });
+});
+
+app.post("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const index = tasks.findIndex((post) => post.id == id);
+  tasks[index] = {
+    id,
+    title,
+  };
+
+  return res.redirect("/");
 });
 
 app.listen(PORT, () => {
-    console.log(`server started on http://localhost:${PORT}.`);
-  });
+  console.log(`server started on http://localhost:${PORT}.`);
+});
